1426de040ccc1846d1edc4406d61929260aa0cdd
Sushil911 <Sushilbasyal30@gmail.com>
2025-11-28 21:57:08 +0545

    basic changes

--- main.go
+++ main.go
@@ -15,36 +15,41 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Chaosboard v0.1 - Chaos Engineering Platform \n")
-		fmt.Fprintf(w, "GET /healthz -> 200 OK \n")
-		fmt.Fprintf(w, "GET /chaos/start -> trigger chaos(coming soon) \n")
+		fmt.Fprintf(w, "Welcome to Chaosboard - v0.1 \n")
+		fmt.Fprintf(w, "GET /healthz - check health of your container \n")
+		fmt.Fprintf(w, "GET /chaos - check the resiliency of your deployed containers \n")
 	})
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("ok"))
+		w.Write([]byte("ok \n"))
+	})
+
+	mux.HandleFunc("/chaos", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Coming soon \n")
 	})
 
 	server := &http.Server{Addr: ":8080", Handler: mux}
+
 	go func() {
 		log.Println("Chaosboard listening on 8080")
 		err := server.ListenAndServe()
 		if err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Server failed %v", err)
+			log.Fatalf("Server start failed %v", err)
 		}
 	}()
 
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	<-sigs
-	log.Println("Shutdown signal received")
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
 
+	log.Println("Shutdown signal received")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-
-	if err := server.Shutdown(ctx); err != nil {
-		log.Printf("Server forced to shutdown: %v", err)
+	err := server.Shutdown(ctx)
+	if err != nil {
+		log.Printf("Shutting down forcefully %v", err)
 	} else {
-		log.Println("Server stopped gracefully")
+		log.Println("Shutting down gracefully")
 	}
 }
